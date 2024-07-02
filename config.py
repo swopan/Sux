c16ee47f79679c8ea6cfb9113bcbfe3ba59fbb2c
Sumitpatel080 <patrlsumit93@gmail.com>
2024-07-02 22:27:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7030354839:AAHMwLENaFZeqhB-yZ6k-EzVmu1HiCLN_Yk")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7279617995:AAFt6zDyJTbf1sa1K2UU6jOfYD7V7l3uDK4")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "23004940"))
@@ -19,16 +19,16 @@ APP_ID = int(os.environ.get("APP_ID", "23004940"))
 API_HASH = os.environ.get("API_HASH", "25c013f901474cffcad6a9d03e3a06bb")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002236755037"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002157052334"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "6314089978"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6367302898"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = "mongodb+srv://sunitverma080:jCOU0WgHZ9z2EEra@verification1.xqjkjto.mongodb.net/?retryWrites=true&w=majority&appName=Verification1"
+DB_URI = "mongodb+srv://sunitverma080:lwYnQJB7WLC50xho@lodubot1.5w14taf.mongodb.net/?retryWrites=true&w=majority&appName=lodubot1"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "inshorturl.com")
@@ -39,7 +39,7 @@ TUT_VID = os.environ.get("TUT_VID","gojfsi/2")
 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002230607811"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "100"))
 
@@ -47,7 +47,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "100"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "7258325900,1709824863").split()):
+    for x in (os.environ.get("ADMINS", "6367302898, 7277357700").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -65,10 +65,10 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "Join Posting Channel From  @Direct_vid For More Links/Files!"
 
-ADMINS.append(7258325900)
-ADMINS.append(7258325900)
+ADMINS.append(6367302898)
+ADMINS.append(7208538115)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
